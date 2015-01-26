33dc1529bf3a89b9871debbf875286df3cee90fe
Marcus McCurdy <marcus.mccurdy@gmail.com>
2015-01-26 12:13:10 -0500

    Adds Rot13

--- Rot13.hs
+++ Rot13.hs
@@ -3,12 +3,14 @@ module Rot13 where
 import Data.Char
 
 rot13 :: String -> String
-rot13 = map (\c -> if isLetter c then rotate c else c)
+rot13 = map rotate
+    where rotate :: Char -> Char
+          rotate c
+            | not $ (isAscii c && isLetter c)       = c
+            | rotated < lowerBound                  = chr $ rotated + lowerBound
+            | otherwise                             = chr rotated
+                where rotated                       = (ord c + 13) `mod` upperBound
+                      lowerBound                    = if isUpper c then ord 'A' else ord 'a'
+                      upperBound                    = (if isUpper c then ord 'Z' else ord 'z') + 1
 
-rotate :: Char -> Char
-rotate c
-    | rotated < lowerBound = chr $ rotated + lowerBound
-    | otherwise            = chr rotated
-        where rotated      = (ord c + 13) `mod` upperBound
-              lowerBound   = if isUpper c then ord 'A' else ord 'a'
-              upperBound   = (if isUpper c then ord 'Z' else ord 'z') + 1
+
